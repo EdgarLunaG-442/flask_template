1aed390f209a3ec0b676a08b9973e24f9ba48dac
EdgarLunaG-442 <ef.luna442@uniandes.edu.co>
2022-03-31 20:12:39 -0500

    modifications for beanstalk

---
+++ application.py
@@ -0,0 +1,3 @@
+from entrypoint import application
+if __name__ == "__main__":
+	application.run(port=8000, debug=True)
--- entrypoint.py
+++ entrypoint.py
@@ -2,7 +2,7 @@ try:
     from api import create_app
 
     settings_module = 'config.default'
-    app = create_app(settings_module)
+    application = create_app(settings_module)
     
 except ImportError as e:
     print(e.msg)
