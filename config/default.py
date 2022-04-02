059b2260415849829572c5041c4f249e09176d27
Gerzon <gerzon.bautista@dafiti.com.co>
2022-04-02 15:20:03 -0500

    Change value LOCAL_DB_FILE

--- config/default.py
+++ config/default.py
@@ -10,7 +10,7 @@ POSTGRES_PWD = os.getenv('POSTGRES_PWD', 'BJM3akVphB3MQcyzrykWxQfbzEwFSm')
 POSTGRES_HOST = os.getenv('POSTGRES_HOST', 'blacklist.csakqsft2dm2.us-east-1.rds.amazonaws.com')
 POSTGRES_PORT = os.getenv('POSTGRES_PORT', '5432')
 POSTGRES_DB = os.getenv('POSTGRES_DB', 'blacklist')
-LOCAL_DB_FILE = os.getenv('LOCAL_DB_FILE', True)
+LOCAL_DB_FILE = os.getenv('LOCAL_DB_FILE')
 
 if LOCAL_DB_FILE:
     SQLALCHEMY_DATABASE_URI = 'sqlite:///../test.db'
