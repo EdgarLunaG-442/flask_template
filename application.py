129bda8567ee8ba1f730cf35d9736969cd26a3da
EdgarLunaG-442 <ef.luna442@uniandes.edu.co>
2022-04-30 14:12:28 -0500

    Se adiciona el docker file y se crea el gunicorn para correr el servicio en modo prod

--- application.py
+++ application.py
@@ -1,12 +1,18 @@
-try:
-    from api import create_app
-    from flask import Flask
+from api import create_app
+from flask import Flask
 
-    settings_module = 'config.default'
+settings_module = 'config.default'
+
+
+def gunicorn():
     application = create_app(settings_module)
+    return application
 
+
+try:
     if __name__ == "__main__":
-        application.run(debug=True)
+        application = create_app(settings_module)
+        application.run(debug=True, host="0.0.0.0")
 
 except ImportError as e:
     print(e.msg)
